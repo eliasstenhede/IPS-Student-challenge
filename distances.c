4dddee5b7d705b54c0f8aa92a0d4fd5db5d4a831
eliasstenhede <72222347+eliasstenhede@users.noreply.github.com>
2022-11-18 20:31:35 +0100

    Change input file path

--- distances.c
+++ distances.c
@@ -16,6 +16,7 @@ int countRows(FILE *fp) {
     return count;
 }
 
+// helper function for mergeSort
 static inline void merge(float arr[], int l, int m, int r){
     int ix, jx, kx;
     int n1 = m - l + 1;
@@ -65,6 +66,7 @@ static inline void merge(float arr[], int l, int m, int r){
     }
 }
 
+// Sorts array of 3D-coordinates in order [x1 y1 z1 x2 y2 z2 x3 ...] by x-values, ascending
 static inline void mergeSort(float arr[], int l, int r) {
     if (l < r) {
 	int m = l + (r - l) / 2;
@@ -74,61 +76,67 @@ static inline void mergeSort(float arr[], int l, int r) {
     }
 }
 
+// Reads input file given for this problem,
+// assuming all coordinates are in the interval (-9.99999, 9.999999) and specified with constant number of digits.
 void parseFile(FILE* fp, float* points) {
     size_t ix = 0;
     char s[31];
     char *e;
-    /* int coord, startp; */
     int startp;
     float coord;
     for (fgets(s, 31, fp); (e = fgets(s, 31, fp)) != NULL; ix+=3) {
         startp=0;
 	for (int jx=0; jx < 3; jx++) {
 	    if (e[startp] == '-') {
-	        coord = -1.f       *(e[startp+1]-'0')
-		        -0.1f      *(e[startp+3]-'0')
-			-0.01f     *(e[startp+4]-'0')
-		        -0.001f    *(e[startp+5]-'0')
-			-0.0001f   *(e[startp+6]-'0')
-			-0.00001f  *(e[startp+7]-'0')
-			-0.000001f *(e[startp+8]-'0');
+	        coord = -1.f      *(e[startp+1]-'0')
+		        -0.1f     *(e[startp+3]-'0')
+			-0.01f    *(e[startp+4]-'0')
+		        -0.001f   *(e[startp+5]-'0')
+			-0.0001f  *(e[startp+6]-'0')
+			-0.00001f *(e[startp+7]-'0')
+			-0.000001f*(e[startp+8]-'0');
 	        startp+=10;
 	    } else {
-	        coord = +1.f       *(e[startp+0]-'0')
-		        +0.1f      *(e[startp+2]-'0')
-			+0.01f     *(e[startp+3]-'0')
-		        +0.001f    *(e[startp+4]-'0')
-			+0.0001f   *(e[startp+5]-'0')
-			+0.00001f  *(e[startp+6]-'0')
-			+0.000001f *(e[startp+7]-'0');
+	        coord = +1.f      *(e[startp+0]-'0')
+		        +0.1f     *(e[startp+2]-'0')
+			+0.01f    *(e[startp+3]-'0')
+		        +0.001f   *(e[startp+4]-'0')
+			+0.0001f  *(e[startp+5]-'0')
+			+0.00001f *(e[startp+6]-'0')
+			+0.000001f*(e[startp+7]-'0');
 	        startp+=9;
             }
             points[ix+jx] = (float) coord;
         }
     }
+    fclose(fp);
 }
 
 
 
 int main(int argc, char *argv[]) {
     //Read file and sort by x-coordinate
-    FILE *fp  = fopen("./points.txt", "r");
+    FILE *fp  = fopen("./positions.xyz", "r");
     const int rows_in_file = countRows(fp);
     float* points = (float*) malloc(rows_in_file*3*sizeof(float));
     parseFile(fp, points);
     mergeSort(points, 0, rows_in_file-1);
+    
     //Set number of threads
     /* const int threads = 1; */
     /* omp_set_num_threads(threads); */
+    
     //Count the points with distance < .05 
     float dx1,dy1,dz1,dx2,dy2,dz2,dx3,dy3,dz3,dx4,dy4,dz4;
     size_t count_close = 0;
     size_t jx;
+    
     /* #pragma omp parallel for schedule(dynamic) reduction(+:count_close) */
     for (size_t ix=0; ix<rows_in_file*3; ix+=3) {
 	jx = ix+3;
-	while(points[jx] - points[ix] < 0.05f && jx < rows_in_file*3)
+	while(points[jx+1] - points[ix+1] < 0.05f && jx < rows_in_file*3)
 	{
+	    //Partially unrolling loop to get multiple of 4 of the operations, which allowed for vectorization on my CPU.
 	    dx1=points[ix+0]-points[jx+0 ];
 	    dy1=points[ix+1]-points[jx+1 ];
 	    dz1=points[ix+2]-points[jx+2 ];
@@ -141,7 +149,7 @@ int main(int argc, char *argv[]) {
 	    dx4=points[ix+0]-points[jx+9 ];
 	    dy4=points[ix+1]-points[jx+10];
 	    dz4=points[ix+2]-points[jx+11];
-	    
+
 	    dx1=dx1*dx1;
 	    dy1=dy1*dy1;
 	    dz1=dz1*dz1;
